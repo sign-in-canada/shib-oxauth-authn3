bef8a1c2afb184f876a0bb7cede58e33f4314bf2
yurem <Yuriy.Movchan@gmail.com>
2019-04-12 12:33:52 +0300

    Persistence annotations should be DB neutral

--- src/main/java/org/gluu/idp/externalauth/openid/conf/IdpLdapAppConfiguration.java
+++ src/main/java/org/gluu/idp/externalauth/openid/conf/IdpLdapAppConfiguration.java
@@ -1,8 +1,8 @@
 package org.gluu.idp.externalauth.openid.conf;
 
 import org.gluu.oxauth.client.conf.LdapAppConfiguration;
-import org.gluu.site.ldap.persistence.annotation.LdapAttribute;
-import org.gluu.site.ldap.persistence.annotation.LdapJsonObject;
+import org.gluu.persist.annotation.AttributeName;
+import org.gluu.persist.annotation.JsonObject;
 
 /**
  * Ldap application configuration model
@@ -14,8 +14,8 @@ public class IdpLdapAppConfiguration extends LdapAppConfiguration {
 
     private static final long serialVersionUID = -7301311833970330177L;
 
-    @LdapJsonObject
-    @LdapAttribute(name = "oxConfApplication")
+    @JsonObject
+    @AttributeName(name = "oxConfApplication")
     private IdpAppConfiguration application;
 
     @Override
