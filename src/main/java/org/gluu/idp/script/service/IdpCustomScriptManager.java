d89add6b00deb5ba0848eaf385328fde1c61d326
yurem <Yuriy.Movchan@gmail.com>
2020-06-18 16:17:34 +0300

    Add Custom Script Manager prototype

---
+++ src/main/java/org/gluu/idp/script/service/IdpCustomScriptManager.java
@@ -0,0 +1,27 @@
+package org.gluu.idp.script.service;
+
+import org.gluu.idp.externalauth.openid.conf.IdpConfigurationFactory;
+import org.gluu.idp.script.service.external.IdpExternalScriptService;
+import org.gluu.service.custom.script.CustomScriptManager;
+
+/**
+ * IDP Custom Script Manager
+ * 
+ * @author Yuriy Movchan
+ * @version 0.1, 06/18/2020
+ */
+public class IdpCustomScriptManager extends CustomScriptManager {
+
+	private static final long serialVersionUID = 2727779937414218627L;
+
+	public IdpCustomScriptManager(final IdpConfigurationFactory configurationFactory) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public IdpExternalScriptService getIdpExternalScriptService() {
+		// TODO Auto-generated constructor stub
+		
+		return null;
+	}
+
+}
