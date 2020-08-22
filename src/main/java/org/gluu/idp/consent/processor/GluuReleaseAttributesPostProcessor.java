6d8eaddf2e9acad5b8eafac66c2a9f38f0b12c53
Harjinder Dhanjal <malotian@gmail.com>
2020-08-22 09:45:21 -0400

    no release-attr dependnecy

--- src/main/java/org/gluu/idp/consent/processor/GluuReleaseAttributesPostProcessor.java
+++ src/main/java/org/gluu/idp/consent/processor/GluuReleaseAttributesPostProcessor.java
@@ -1,21 +1,23 @@
 package org.gluu.idp.consent.processor;
 
+import java.util.function.Function;
+
 import javax.annotation.Nonnull;
 
-import org.gluu.idp.consent.processor.PostProcessAttributesContext;
 import org.gluu.idp.externalauth.openid.conf.IdpConfigurationFactory;
 import org.gluu.idp.script.service.IdpCustomScriptManager;
 import org.gluu.idp.script.service.external.IdpExternalScriptService;
+import org.opensaml.messaging.context.navigate.ChildContextLookup;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.shibboleth.idp.attribute.context.AttributeContext;
-import net.shibboleth.idp.consent.flow.ar.impl.AbstractAttributeReleaseAction;
-import net.shibboleth.idp.profile.context.ProfileInterceptorContext;
+import net.shibboleth.idp.profile.AbstractProfileAction;
+import net.shibboleth.idp.profile.context.RelyingPartyContext;
 import net.shibboleth.utilities.java.support.logic.Constraint;
 
-public class GluuReleaseAttributesPostProcessor extends AbstractAttributeReleaseAction {
+public class GluuReleaseAttributesPostProcessor extends AbstractProfileAction {
 
 	private final Logger LOG = LoggerFactory.getLogger(GluuReleaseAttributesPostProcessor.class);
 
@@ -23,52 +25,62 @@ public class GluuReleaseAttributesPostProcessor extends AbstractAttributeRelease
 	private IdpCustomScriptManager customScriptManager;
 	private IdpExternalScriptService externalScriptService;
 
-    public GluuReleaseAttributesPostProcessor() {
-    	
-    	configurationFactory = IdpConfigurationFactory.instance(); 
-    	customScriptManager = new IdpCustomScriptManager(configurationFactory, true);
-     
-    	LOG.debug("ReleaseAttributesPostProcessor: create");
-        Constraint.isNotNull(configurationFactory, "Configuration factory cannot be null");
-        Constraint.isNotNull(customScriptManager, "Custom script manager cannot be null");
- 
-        init();
-    }
+	public GluuReleaseAttributesPostProcessor() {
 
-    private void init() {
-    	// Call custom script manager init to make sure that it initialized
-    	this.customScriptManager.init();
-    	this.externalScriptService = this.customScriptManager.getIdpExternalScriptService();
+		configurationFactory = IdpConfigurationFactory.instance();
+		customScriptManager = new IdpCustomScriptManager(configurationFactory, true);
+
+		LOG.debug("ReleaseAttributesPostProcessor: create");
+		Constraint.isNotNull(configurationFactory, "Configuration factory cannot be null");
+		Constraint.isNotNull(customScriptManager, "Custom script manager cannot be null");
+
+		init();
+	}
+
+	private void init() {
+		// Call custom script manager init to make sure that it initialized
+		this.customScriptManager.init();
+		this.externalScriptService = this.customScriptManager.getIdpExternalScriptService();
 	}
 
 	/**
-     * Performs this profile interceptor action. Default implementation does nothing.
-     * 
-     * @param profileRequestContext the current profile request context
-     * @param interceptorContext the current profile interceptor context
-     */
+	 * Performs this profile interceptor action. Default implementation does
+	 * nothing.
+	 * 
+	 * @param profileRequestContext the current profile request context
+	 * @param interceptorContext    the current profile interceptor context
+	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	protected void doExecute(@Nonnull ProfileRequestContext profileRequestContext, @Nonnull ProfileInterceptorContext interceptorContext) {
+	protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
 		// Execute default flow first
 		LOG.info("Executing external IDP script");
-		super.doExecute(profileRequestContext, interceptorContext);
+		super.doExecute(profileRequestContext);
+
+		PostProcessAttributesContext context = buildContext(profileRequestContext);
+
+		AttributeContext attributeContext = context.getAttributeContext();
+		for (String attr : attributeContext.getIdPAttributes().keySet()) {
+			LOG.info("------------------------attr: {}", attr);
+		}
 
 		// Return if script(s) not exists or invalid
 		if (!this.externalScriptService.isEnabled()) {
-			LOG.trace("Using default release attributes post processor");
+			LOG.info("Using default release attributes post processor");
 			return;
 		}
-		
-		LOG.info("Executing external IDP script");
-		PostProcessAttributesContext context = buildContext();
+
 		boolean result = this.externalScriptService.executeExternalUpdateAttributesMethod(context);
 
 		LOG.debug("Executed script method 'updateAttributes' with result {}", result);
 	}
 
-	private PostProcessAttributesContext buildContext() {
-		AttributeContext attributeContext = getAttributeContext();
+	private PostProcessAttributesContext buildContext(final ProfileRequestContext profileRequestContext) {
+
+		Function<ProfileRequestContext, AttributeContext> attributeContextLookupStrategy = null;
+		attributeContextLookupStrategy = new ChildContextLookup<>(AttributeContext.class).compose(new ChildContextLookup<>(RelyingPartyContext.class));
+
+		AttributeContext attributeContext = attributeContextLookupStrategy.apply(profileRequestContext);
 
 		PostProcessAttributesContext context = new PostProcessAttributesContext();
 		context.setAttributeContext(attributeContext);
